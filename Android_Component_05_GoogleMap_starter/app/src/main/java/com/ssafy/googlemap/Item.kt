2d2ddc51fb9c1c73a47633529e1baf8475d55ee7
오승연 <fltkfltk1004@naver.com>
2025-04-17 17:07:14 +0900

    no message

--- Android_Component_05_GoogleMap_starter/app/src/main/java/com/ssafy/googlemap/Item.kt
+++
@@ -1,7 +0,0 @@
-package com.ssafy.googlemap
-
-data class Item(val type:String, val title:String, val latitude:Double, val longitude:Double)
-
-enum class Type{
-    RESTAURANT, BANK, CAFE
-}
--- ws_android_component_map/app/src/main/java/com/ssafy/cleanstore/dto/Stuff.kt
+++
@@ -1,9 +0,0 @@
-package com.ssafy.cleanstore.dto
-
-import java.io.Serializable
-
-data class Stuff(val id:Int, val name:String, val count:Int):Serializable {
-    override fun toString(): String {
-        return "물품 : $name -> 수량 : $count"
-    }
-}
